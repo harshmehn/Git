e7bb151ee1c0b72ff45781bc90cef021c1171ca8
harshmehn <harshitmehndiratta1999@gmail.com>
2019-03-07 12:26:54 +0530

    Drag and drop edit and button and set a click listener on button afet click a button saw a toast

--- app/src/main/java/com/example/git/MainActivity.java
+++ app/src/main/java/com/example/git/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.git;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    
+EditText editText;
+
+Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText=findViewById(R.id.editText);
+
+        button=findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String ed=editText.getText().toString();
+                Toast.makeText(MainActivity.this, ""+ed, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
